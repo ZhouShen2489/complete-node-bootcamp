25a028e8d2e698193030e89c5c7f56dfc50cce2b
ZhouShen2489 <zs2489@columbia.edu>
2024-03-27 15:19:46 -0700

    learned HTML Template

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -24,23 +24,68 @@ const url = require("url");
 ///////////////////////
 // SERVER & ROUTING
 
+const replaceCard = (temp, card) => {
+  let output = temp.replace(/{%IMAGE%}/g, card.image);
+  output = output.replace(/{%PRODUCTNAME%}/g, card.productName);
+  output = output.replace(/{%ID%}/g, card.id);
+  output = output.replace(/{%FROM%}/g, card.from);
+  output = output.replace(/{%NUTRIENTS%}/g, card.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, card.quantity);
+  output = output.replace(/{%PRICE%}/g, card.price);
+
+  if (!card.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+
 // only get and parse the data once at the beginning
+// HTML Templating
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const productInfo = JSON.parse(data);
+const dataObj = JSON.parse(data);
+// console.log(productInfo);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
+
+  // Overview Page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW!");
-  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardHTML = dataObj
+      .map((el) => replaceCard(templateCard, el))
+      .join("");
+    const output = templateOverview.replace("{%PRODUCT_CARDS%}", cardHTML);
+
+    res.end(output);
+  }
+
+  // Product Page
+  else if (pathName === "/api") {
     // parse the json, get product info
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
   }
+
+  // API
   // res.end("API");
-  else if (pathName === "/products") {
+  else if (pathName === "/product") {
     res.end("This is the PRODUCT!");
-  } else {
+  }
+
+  // Not Found
+  else {
     // response headers must be in front of the res.end. Header can includes some meta data
     res.writeHead(404, {
       "Content-type": "text/html",
