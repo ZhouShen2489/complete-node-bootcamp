88917fbbeaf03232a440ec7775ccf58f7032befe
ZhouShen2489 <zs2489@columbia.edu>
2024-04-02 14:19:12 -0700

    learn the async/await, complete this section

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -19,23 +19,48 @@ const writeFilePro = (file, data) => {
   });
 };
 
+// async automatically return a promise
 const getDogPic = async () => {
   try {
-    const data = await readFilePro(`${__dirname}/doggg.txt`);
+    const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
-    const res = await superagent.get(
+    const res1Pro = superagent.get(
       `https://dog.ceo/api/breed/${data}/images/random`
     );
-    console.log(res.body.message);
-    await writeFilePro('dog-img.txt', res.body.message);
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    // put three images together once they all resolved
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const allImgs = all.map((el) => el.body.message);
+    console.log(allImgs);
+    await writeFilePro('dog-img.txt', allImgs.join('\n'));
     console.log('Random Dog Image saved to file!');
   } catch (err) {
     console.log(err);
-    throw err;
+    throw err; // throw a err before return anything
   }
   return '2: Ready 🐶';
 };
 
+// Uses async/await to apply async/await
+(async () => {
+  try {
+    console.log('1: Getting dog pic...');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Done getting dog pic!');
+  } catch (err) {
+    console.log('Error 💥');
+  }
+})();
+
+/*
+// Use then, catch to apply async/await
 console.log('1: Getting dog pic...');
 getDogPic()
   .then((x) => {
@@ -43,6 +68,7 @@ getDogPic()
     console.log('3: Done getting dog pic!');
   })
   .catch((err) => console.log('Error 💥'));
+  */
 
 /*
 readFilePro(`${__dirname}/dog.txt`)
