74fc3181dfe64e32c7bb975ccb49cf7941710961
ZhouShen2489 <zs2489@columbia.edu>
2024-04-03 12:01:53 -0700

    learn postman, express get request

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).send({
+    status: 'success',
+    result: tours.length,
+    tours: tours,
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
